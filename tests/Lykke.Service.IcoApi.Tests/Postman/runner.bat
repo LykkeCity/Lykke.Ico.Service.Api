785a65e46c8c89858861979de845cbd4bf48e40c
Andrey Krivoshapov <andrey.krivoshapov@lykke.com>
2017-12-10 14:50:47 +0300

    Update core lib

--- tests/Lykke.Service.IcoApi.Tests/Postman/runner.bat
+++ tests/Lykke.Service.IcoApi.Tests/Postman/runner.bat
@@ -1,1 +1,2 @@
 newman run valid-ico-happy-flow.postman_collection.json -e dev.postman_environment.json
+newman run valid-ico-real-crypto-transactions-flow.postman_collection.json -e dev.postman_environment.json
