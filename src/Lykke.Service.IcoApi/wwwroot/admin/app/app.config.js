39a725f01e902eb71e6b72648310166bfc14cbda
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2018-03-01 17:23:31 +0300

    Added ability to edit time

--- src/Lykke.Service.IcoApi/wwwroot/admin/app/app.config.js
+++ src/Lykke.Service.IcoApi/wwwroot/admin/app/app.config.js
@@ -1,4 +1,49 @@
 import { app, AppEvent, AppToastType } from "./app.js";
+const regexIso8601 = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/;
+function transformDateStringsToDates(input) {
+    if (typeof input !== "object") {
+        return input;
+    }
+    for (var key in input) {
+        if (!input.hasOwnProperty(key)) {
+            continue;
+        }
+        var value = input[key];
+        var match;
+        if (typeof value === "string" && (match = value.match(regexIso8601))) {
+            var milliseconds = Date.parse(match[0]);
+            if (!isNaN(milliseconds)) {
+                input[key] = new Date(milliseconds);
+            }
+        }
+        else if (typeof value === "object") {
+            input[key] = transformDateStringsToDates(value);
+        }
+    }
+    return input;
+}
+function interceptAuthTokensAndHttpErrors($q, $rootScope, authUtils) {
+    return {
+        request: request => {
+            if (!!authUtils.authToken) {
+                request.headers["adminAuthToken"] = authUtils.authToken;
+            }
+            return request;
+        },
+        responseError: response => {
+            if (!authUtils.isAuthorized(response)) {
+                $rootScope.$emit(AppEvent.ReloadRoute);
+            }
+            else if (!authUtils.isLoginForbidden(response)) {
+                $rootScope.$emit(AppEvent.Toast, {
+                    message: (response.data && response.data.Message) || response.statusText || "Technical problem",
+                    type: AppToastType.Error
+                });
+            }
+            return $q.reject(response);
+        }
+    };
+}
 const appRoutes = [
     { link: "campaign-info", icon: "info", name: "Info", template: "<campaign-info></campaign-info>" },
     { link: "campaign-email-templates", icon: "email", name: "Email Templates", template: "<campaign-email-templates></campaign-email-templates>" },
@@ -20,30 +65,11 @@ app.config(($routeProvider, $locationProvider, appRoutes) => {
         $routeProvider.when(`/${route.link}`, route);
     });
 });
-// config interceptors
+// config interceptors and transformers
 app.config(($httpProvider) => {
-    $httpProvider.interceptors.push(($q, $rootScope, authUtils) => {
-        return {
-            request: request => {
-                if (!!authUtils.authToken) {
-                    request.headers["adminAuthToken"] = authUtils.authToken;
-                }
-                return request;
-            },
-            responseError: response => {
-                if (!authUtils.isAuthorized(response)) {
-                    $rootScope.$emit(AppEvent.ReloadRoute);
-                }
-                else if (!authUtils.isLoginForbidden(response)) {
-                    $rootScope.$emit(AppEvent.Toast, {
-                        message: (response.data && response.data.Message) || response.statusText || "Technical problem",
-                        type: AppToastType.Error
-                    });
-                }
-                return $q.reject(response);
-            }
-        };
-    });
+    let transformers = angular.isArray($httpProvider.defaults.transformResponse) ? $httpProvider.defaults.transformResponse : [$httpProvider.defaults.transformResponse];
+    $httpProvider.defaults.transformResponse = transformers.concat(transformDateStringsToDates);
+    $httpProvider.interceptors.push(interceptAuthTokensAndHttpErrors);
 });
 // config Material Design
 app.config(($mdThemingProvider, $mdAriaProvider) => {
--- src/Lykke.Service.IcoApi/wwwroot/admin/app/app.config.ts
+++ src/Lykke.Service.IcoApi/wwwroot/admin/app/app.config.ts
@@ -1,6 +1,57 @@
 ﻿import { app, IAppRoute, AppEvent, AppToastType } from "./app.js";
 import { AuthService, AuthUtils  } from "./auth/auth.js";
 
+const regexIso8601 = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/;
+
+function transformDateStringsToDates(input) {
+    if (typeof input !== "object") {
+        return input;
+    }
+
+    for (var key in input) {
+        if (!input.hasOwnProperty(key)) {
+            continue;
+        }
+
+        var value = input[key];
+        var match;
+
+        if (typeof value === "string" && (match = value.match(regexIso8601))) {
+            var milliseconds = Date.parse(match[0])
+            if (!isNaN(milliseconds)) {
+                input[key] = new Date(milliseconds);
+            }
+        } else if (typeof value === "object") {
+            input[key] = transformDateStringsToDates(value);
+        }
+    }
+
+    return input;
+}
+
+function interceptAuthTokensAndHttpErrors($q: ng.IQService, $rootScope: ng.IRootScopeService, authUtils: AuthUtils) {
+    return {
+        request: request => {
+            if (!!authUtils.authToken) {
+                request.headers["adminAuthToken"] = authUtils.authToken;
+            }
+            return request;
+        },
+        responseError: response => {
+            if (!authUtils.isAuthorized(response)) {
+                $rootScope.$emit(AppEvent.ReloadRoute);
+            } else if (!authUtils.isLoginForbidden(response)) {
+                $rootScope.$emit(AppEvent.Toast, {
+                    message: (response.data && response.data.Message) || response.statusText || "Technical problem",
+                    type: AppToastType.Error
+                });
+            }
+
+            return $q.reject(response);
+        }
+    };
+}
+
 const appRoutes: IAppRoute[] = [
     { link: "campaign-info", icon: "info", name: "Info", template: "<campaign-info></campaign-info>" },
     { link: "campaign-email-templates", icon: "email", name: "Email Templates", template: "<campaign-email-templates></campaign-email-templates>" },
@@ -25,30 +76,11 @@ app.config(($routeProvider: ng.route.IRouteProvider, $locationProvider: ng.ILoca
     });
 });
 
-// config interceptors
+// config interceptors and transformers
 app.config(($httpProvider: ng.IHttpProvider) => {
-    $httpProvider.interceptors.push(($q: ng.IQService, $rootScope: ng.IRootScopeService, authUtils: AuthUtils) => {
-        return {
-            request: request => {
-                if (!!authUtils.authToken) {
-                    request.headers["adminAuthToken"] = authUtils.authToken;
-                }
-                return request;
-            },
-            responseError: response => {
-                if (!authUtils.isAuthorized(response)) {
-                    $rootScope.$emit(AppEvent.ReloadRoute);
-                } else if (!authUtils.isLoginForbidden(response)) {
-                    $rootScope.$emit(AppEvent.Toast, {
-                        message: (response.data && response.data.Message) || response.statusText || "Technical problem",
-                        type: AppToastType.Error
-                    });
-                }
-                
-                return $q.reject(response);
-            }
-        };
-    });
+    let transformers = angular.isArray($httpProvider.defaults.transformResponse) ? $httpProvider.defaults.transformResponse : [$httpProvider.defaults.transformResponse];
+    $httpProvider.defaults.transformResponse = transformers.concat(transformDateStringsToDates);
+    $httpProvider.interceptors.push(interceptAuthTokensAndHttpErrors);
 });
 
 // config Material Design
