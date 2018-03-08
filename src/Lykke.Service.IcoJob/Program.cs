fd8718ca4bb9221fc96e6fd5232d5b3d98b00515
Andrey Krivoshapov <andrey.krivoshapov@lykke.com>
2018-03-08 15:14:39 +0300

    Update postman scripts

--- src/Lykke.Service.IcoJob/Program.cs
+++ src/Lykke.Service.IcoJob/Program.cs
@@ -9,7 +9,7 @@ namespace Lykke.Service.IcoJob
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine($"IcoApi version {Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationVersion}");
+            Console.WriteLine($"IcoJob version {Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationVersion}");
 #if DEBUG
             Console.WriteLine("Is DEBUG");
 #else
