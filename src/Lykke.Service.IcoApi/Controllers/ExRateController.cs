3e7af8c2ad14a9f27320d094089286db7188c652
Andrey Krivoshapov <andrey.krivoshapov@lykke.com>
2018-05-30 14:45:55 +0300

    Update /api/exrate/{pair} endpoint

--- src/Lykke.Service.IcoApi/Controllers/ExRateController.cs
+++ src/Lykke.Service.IcoApi/Controllers/ExRateController.cs
@@ -1,4 +1,5 @@
-﻿using Lykke.Service.IcoApi.Models;
+﻿using Lykke.Service.IcoApi.Core.Services;
+using Lykke.Service.IcoApi.Models;
 using Lykke.Service.IcoExRate.Client;
 using Lykke.Service.IcoExRate.Client.AutorestClient.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -13,12 +14,14 @@ namespace Lykke.Service.IcoApi.Controllers
     public class ExRateController : Controller
     {
         private readonly IIcoExRateClient _icoExRateClient;
+        private readonly ICampaignService _campaignService;
         private readonly IMemoryCache _cache;
-        private readonly string _key = "LatestExRate";
 
         public ExRateController(IIcoExRateClient icoExRateClient,
+            ICampaignService campaignService,
             IMemoryCache cache)
         {
+            _campaignService = campaignService;
             _icoExRateClient = icoExRateClient;
             _cache = cache;
         }
@@ -29,7 +32,9 @@ namespace Lykke.Service.IcoApi.Controllers
         [HttpGet("{assetPair}")]
         public async Task<AverageRateResponse> GetLatestRate([Required] AssetPair assetPair)
         {
-            var response = _cache.Get<AverageRateResponse>(_key);
+            var key = $"LatestExRate_{Enum.GetName(typeof(AssetPair), assetPair)}";
+
+            var response = _cache.Get<AverageRateResponse>(key);
             if (response != null)
             {
                 return response;
@@ -37,8 +42,25 @@ namespace Lykke.Service.IcoApi.Controllers
 
             var pair = Enum.Parse<Pair>(Enum.GetName(typeof(AssetPair), assetPair), true);
             var rate = await _icoExRateClient.GetAverageRate(pair, DateTime.UtcNow);
+            var settings = await _campaignService.GetCampaignSettings();
 
-            _cache.Set(_key, response, DateTimeOffset.Now.AddSeconds(10));
+            if (pair == Pair.ETHUSD &&
+                rate.AverageRate.HasValue &&
+                settings.MinEthExchangeRate.HasValue &&
+                rate.AverageRate.Value < (double)settings.MinEthExchangeRate.Value)
+            {
+                rate.AverageRate = (double)settings.MinEthExchangeRate.Value;
+            }
+
+            if (pair == Pair.BTCUSD &&
+                rate.AverageRate.HasValue &&
+                settings.MinBtcExchangeRate.HasValue &&
+                rate.AverageRate.Value < (double)settings.MinBtcExchangeRate.Value)
+            {
+                rate.AverageRate = (double)settings.MinBtcExchangeRate.Value;
+            }
+
+            _cache.Set(key, rate, DateTimeOffset.Now.AddSeconds(5));
 
             return rate;
         }
